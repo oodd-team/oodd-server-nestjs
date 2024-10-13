903bddfc3d32d35627ff064b241a0991cdd38da5
khmandarrin <127952295+khmandarrin@users.noreply.github.com>
2024-10-14 07:53:01 +0900

    fix: 오류 수정

--- src/post-image/post-image.service.ts
+++ src/post-image/post-image.service.ts
@@ -1,33 +1,3 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { PostImage } from 'src/common/entities/post-image.entity';
-import { Repository } from 'typeorm';
-import { Post } from 'src/common/entities/post.entity';
-import { UploadImageDto } from 'src/post/dtos/create-post.dto';
-import { InternalServerException } from 'src/common/exception/service.exception';
-
 @Injectable()
-export class PostImageService {
-  constructor(
-    @InjectRepository(PostImage)
-    private readonly postImageRepository: Repository<PostImage>,
-  ) {}
-
-  async savePostImages(postImages: UploadImageDto[], post: Post) {
-    if (postImages?.length) {
-      const postImageEntities = postImages.map((image: UploadImageDto) => {
-        return this.postImageRepository.create({
-          url: image.imageurl,
-          orderNum: image.orderNum,
-          post: post,
-        });
-      });
-
-      try {
-        await this.postImageRepository.save(postImageEntities);
-      } catch (error) {
-        throw InternalServerException('image 저장에 실패했습니다.');
-      }
-    }
-  }
-}
+export class PostImageService {}
