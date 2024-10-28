2848f56449b50efdd8fee6d113db04eb93113630
unknown <jisomnie@gmail.com>
2024-10-28 17:01:44 +0900

    feat: 게시물 신고

--- src/post-report/post-report.controller.ts
+++ src/post-report/post-report.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { PostReportService } from './post-report.service';
 import { CreatePostReportSwagger } from './post-report.swagger';
 import { ApiTags } from '@nestjs/swagger';
+import { PostReportDto } from './dtos/post-report.dto';
+import { BaseResponse } from 'src/common/response/dto';
 
 @Controller('post-report')
 @ApiTags('[서비스] 게시글 신고')
@@ -9,7 +11,17 @@ export class PostReportController {
   constructor(private readonly postReportService: PostReportService) {}
   @Post()
   @CreatePostReportSwagger('게시글 신고하기 API')
-  createPostReport() {
-    // return this.userService.getHello();
+  async createPostReport(
+    @Body() postReportDto: PostReportDto,
+    @Req() req: any 
+  ): Promise<BaseResponse<string>> {
+    const requesterId = 1; // 추후 수정
+    
+    await this.postReportService.reportPost({
+      ...postReportDto,
+      requesterId,
+    });
+
+    return new BaseResponse(true, 'SUCCESS', '신고 완료');
   }
 }
--- src/post-report/post-report.module.ts
+++ src/post-report/post-report.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PostReportController } from './post-report.controller';
 import { PostReportService } from './post-report.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostReport } from 'src/common/entities/post-report.entity';
+import { PostModule } from 'src/post/post.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([PostReport]), 
+    PostModule,
+  ],
   controllers: [PostReportController],
   providers: [PostReportService]
 })
