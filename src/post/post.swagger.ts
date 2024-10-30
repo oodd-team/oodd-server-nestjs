e32d84adbada82a3986a83f3a8527b675eebd6bb
khmandarrin <127952295+khmandarrin@users.noreply.github.com>
2024-10-31 05:27:21 +0900

    feat: 게시글 수정 기능

--- src/post/post.swagger.ts
+++ src/post/post.swagger.ts
@@ -121,10 +121,6 @@ export function PatchPostSwagger(text: string) {
         description: '권한이 없습니다.',
         type: BaseResponse,
       }),
-      ApiUnprocessableEntityResponse({
-        description: '요청이 처리 불가능합니다.',
-        type: BaseResponse,
-      }),
       ApiInternalServerErrorResponse({
         description: '서버 오류입니다.',
         type: BaseResponse,
