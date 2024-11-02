0904f15ddd2fb276caf4d1a8cff5a882f7808a59
Minseo Lim <limms1217@naver.com>
2024-11-02 14:51:50 +0900

    feat: cors -에러 해결

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new ServiceExceptionToHttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://oodd.today',
+      'https://dev.oodd.today',
+    ], // 프론트엔드에서 접근 가능한 도메인
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   setupSwagger(app);
   await app.listen(process.env.PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
