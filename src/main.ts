44e9537f43b54751bcc9caaac29a78b06fd32a86
Minseo Lim <limms1217@naver.com>
2024-11-02 17:57:19 +0900

    24.11.02

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new ServiceExceptionToHttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: '*', // 프론트엔드에서 접근 가능한 도메인
-    credentials: true,
-    allowedHeaders: '*', // 모든 헤더 허용
-  });
   setupSwagger(app);
   await app.listen(process.env.PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
