c44d42e3e199c611f19947ed78bdb2c9a6443bbb
Minseo Lim <limms1217@naver.com>
2024-11-05 23:45:42 +0900

    feat: 소켓 완료

--- src/chat-message/chat-message.service.ts
+++ src/chat-message/chat-message.service.ts
@@ -12,6 +12,47 @@ export class ChatMessageService {
     private readonly chatMessageRepository: Repository<ChatMessage>,
   ) {}
 
+  async saveMessage(
+    chatRoomId: number,
+    toUserId: number,
+    message: string,
+    fromUserId: number,
+    createdAt: string,
+  ) {
+    return await this.chatMessageRepository.save({
+      chatRoomId,
+      toUserId,
+      message,
+      fromUserId,
+      createdAt,
+    });
+  }
+
+  async getMessagesByChatRoomId(chatRoomId: number) {
+    const messages = await this.chatMessageRepository.find({
+      where: { chatRoom: { id: chatRoomId }, status: 'activated' },
+      order: { createdAt: 'ASC' }, // 오래된 메시지부터 최신 메시지 순으로 정렬
+      relations: ['fromUser', 'toUser'], // 메시지 발신자, 수신자 정보도 함께 로드
+    });
+
+    return messages.map((message) => ({
+      id: message.id,
+      content: message.content,
+      fromUser: {
+        id: message.fromUser.id,
+        nickname: message.fromUser.nickname,
+        profileUrl: message.fromUser.profilePictureUrl,
+      },
+      toUser: {
+        id: message.toUser.id,
+        nickname: message.toUser.nickname,
+        profileUrl: message.toUser.profilePictureUrl,
+      },
+      createdAt: message.createdAt,
+      toUserReadAt: message.toUserReadAt,
+    }));
+  }
+
   async createChatMessage(
     queryRunner: QueryRunner,
     chatRoom: ChatRoom,
