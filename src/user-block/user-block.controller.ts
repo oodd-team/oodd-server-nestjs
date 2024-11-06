7bc1d4a121a6b17b7d568c357137c97a13749dd9
unknown <jisomnie@gmail.com>
2024-11-06 13:33:38 +0900

    유저 차단 jwt 추가

--- src/user-block/user-block.controller.ts
+++ src/user-block/user-block.controller.ts
@@ -1,24 +1,30 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { UserBlockService } from './user-block.service';
 import { CreateBlockUserSwagger } from './user-block.swagget';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CreateUserBlockDto } from './dtos/user-block.dto';
 import { BaseResponse } from 'src/common/response/dto';
+import { AuthGuard } from 'src/auth/guards/jwt.auth.guard';
+import { Request } from 'express';
+import { UnauthorizedException } from 'src/common/exception/service.exception';
 
 @ApiBearerAuth('Authorization')
 @Controller('user-block')
+@UseGuards(AuthGuard)
 @ApiTags('[서비스] 유저 차단')
 export class UserBlockController {
   constructor(private readonly userBlockService: UserBlockService) {}
 
   @Post()
   @CreateBlockUserSwagger('유저 차단하기 API')
-  async createUserBlock(@Body() createUserBlockDto: CreateUserBlockDto,
-): Promise<BaseResponse<null>> {
-  const fromUserId = 1; // 추후 수정
-  createUserBlockDto.fromUserId = fromUserId;
+  async createUserBlock(
+    @Body() createUserBlockDto: CreateUserBlockDto,
+    @Req() req: Request, 
+  ): Promise<BaseResponse<null>> {
+    const fromUserId = req.user['id']; 
+    //console.log('fromUserId is ', fromUserId);
 
-  await this.userBlockService.createBlock(createUserBlockDto);
+    createUserBlockDto.fromUserId = fromUserId;
 
   const resultCode = await this.userBlockService.createBlock(createUserBlockDto);
 
