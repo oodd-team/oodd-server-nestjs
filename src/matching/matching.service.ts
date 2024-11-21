13c429b654d2523fccdd1abaa871cb012929875e
khmandarrin <127952295+khmandarrin@users.noreply.github.com>
2024-11-22 06:25:42 +0900

    isMatching 수정

--- src/matching/matching.service.ts
+++ src/matching/matching.service.ts
@@ -106,7 +106,7 @@ export class MatchingService {
       .getMany();
 
     const response: GetMatchingsResponse = {
-      isMatching: true,
+      isMatching: matchings.length > 0,
       matchingCount: matchings.length,
       matching: matchings.map((matching) => {
         const requesterPost = matching.requester.posts[0];
