7ab03b668ffedcfc58ec59b031d3958d3d20d8c3
Minseo Lim <limms1217@naver.com>
2024-12-16 03:10:22 +0900

    feat: fix dev deploy

--- prod-scripts/after-deploy.sh
+++ prod-scripts/after-deploy.sh
@@ -4,15 +4,13 @@ APP_NAME=node_app_prod
 
 cd $REPOSITORY
 
-sudo -s
-
 # Check if the app is already running
-if sudo pm2 list | grep $APP_NAME > /dev/null
+if pm2 list | grep $APP_NAME > /dev/null
 then
   echo "$APP_NAME is already running. Restarting..."
-  sudo pm2 delete $APP_NAME
-  sudo pm2 start dist/main.js --name $APP_NAME
+  pm2 delete $APP_NAME
+  pm2 start dist/main.js --name $APP_NAME
 else
   echo "Starting $APP_NAME"
-  sudo pm2 start dist/main.js --name $APP_NAME
+  pm2 start dist/main.js --name $APP_NAME
 fi
