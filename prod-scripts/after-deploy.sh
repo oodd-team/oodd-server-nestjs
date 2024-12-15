363758d3839f9e7850b8feedaa8f42d7e83598cb
Minseo Lim <limms1217@naver.com>
2024-12-16 03:43:23 +0900

    feat: env 수정

--- prod-scripts/after-deploy.sh
+++ prod-scripts/after-deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 REPOSITORY=/home/ubuntu/build
 APP_NAME=node_app_prod
 cd $REPOSITORY
-export PATH=$PATH:$(npm root -g)
 
 # Check if the app is already running
 if pm2 list | grep $APP_NAME > /dev/null
